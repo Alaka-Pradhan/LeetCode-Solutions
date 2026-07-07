a1e13a3cb573b54da886b959f7c8c1892abb0195
Alaka Pradhan <pradhanalaka1976@gmail.com>
2026-07-07 12:56:18 +0530

    Time: 26 ms (50.85%), Space: 12 MB (40.19%) - LeetHub

---
+++ 0147-insertion-sort-list/0147-insertion-sort-list.c
@@ -0,0 +1,23 @@
+struct ListNode* insertionSortList(struct ListNode* head) {
+    struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
+    dummy->val = 0;
+    dummy->next = NULL;
+
+    struct ListNode* curr = head;
+
+    while (curr != NULL) {
+        struct ListNode* next = curr->next;
+
+        struct ListNode* prev = dummy;
+        while (prev->next != NULL && prev->next->val < curr->val) {
+            prev = prev->next;
+        }
+
+        curr->next = prev->next;
+        prev->next = curr;
+
+        curr = next;
+    }
+
+    return dummy->next;
+}
